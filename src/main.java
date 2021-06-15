e3d6bd98ca69640c9b09d1b5d46115deca602d25
Mohammed <mdrazz9@gmail.com>
2021-06-15 10:04:40 +0100

    refactored to add user input

--- src/main.java
+++ src/main.java
@@ -10,21 +10,31 @@ public class main {
             int previousOrder = 715;
             System.out.println(thanks + name.toUpperCase());
             System.out.println(order + (previousOrder+1)); */
-            paintNeeded(5,3);
-            Scanner wall = new Scanner(System.in);
+            
+             Scanner wall = new Scanner(System.in);
+             System.out.println("enter height in meters: ");
+             int wallheight = wall.nextInt();
+
+             System.out.println("Enter width in meters: ");
+             int wallwidth = wall.nextInt();
+
+             System.out.println("Enter number of walls: ");
+             int numofwalls = wall.nextInt();
+
+             System.out.println("Do you have windows and doors?:");
+             boolean windows = wall.nextBoolean();
+
+             
+
+             int area = wallheight * wallwidth;
+             System.out.println("The total area of your wall is: " + area + " m^2 per wall" + " a total of: " + (numofwalls*area));
+             float estimatedPaint = (float) area / 12;
+             System.out.println("With the estimate of 1 litre of paint covering 12 m^2, you will require: " + estimatedPaint +"litres of paint");
+            
 
         }
 
-         static void paintNeeded(int height, int width){
-             //Scanner wall = new Scanner(System.in);
-             // System.out.println("enter height")
-             // int wallheight = wall
-
-             int area = height * width;
-             System.out.println("The total area of your wall is: " + area + " m^2 per wall");
-             float estimatedPaint = (float) area / 12;
-             System.out.println("With the estimate of 1 litre of paint covering 12 m^2, you will require: " + estimatedPaint + " litres of paint");
-            }
+    
 
 
 
